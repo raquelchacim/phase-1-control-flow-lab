0c319b222ee740f54d71b80ae2e50606aa917abe
Raquel <raquel.chacim@hotmail.com>
2022-10-28 15:10:27 +1100

    stuff

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(price) {
+  if (price < 400) {
+    console.log("this one is on me");
+  } else {
+    console.log("this one is not on me");
+  }
 }
+scuberGreetingForFeet(200);
+scuberGreetingForFeet(800);
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+// function ternaryCheckCity() {
+//   // Write your code here!
+// }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+// function switchOnCharmFromTip() {
+//   // Write your code here!
+// }
